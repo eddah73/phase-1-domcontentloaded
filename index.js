60efa3e24c6b03987b03ed4c9fe2f6eeda396b23
eddah73 <eddahsjr@gmail.com>
2024-07-06 17:30:58 +0300

    DOMLoadContent and Load

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let textLoaded = document.getElementById('text');
+    textLoaded.textContent = "This is really cool!"
+   
+  });
+  
